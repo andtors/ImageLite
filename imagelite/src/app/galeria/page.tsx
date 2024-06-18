ee47d224595a83058f8fd0de68526658d72ab7c4
André Torres <150715327+andtors@users.noreply.github.com>
2024-06-18 20:58:48 -0300

    SC-4-Concluido

--- imagelite/src/app/galeria/page.tsx
+++ imagelite/src/app/galeria/page.tsx
@@ -1,36 +1,61 @@
 'use client'
 
 import { Template, ImageCard } from "@/components"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { useImageService } from '@/resources/image/image.service'
+import { Image } from "@/resources/image/image.resource"
 
 const GaleriaPage = () => {
 
+  const useService = useImageService()
+  const [images, setImages] = useState<Image[]>([])
+  const [query, setQuery] = useState<string>('')
+  const [extension, setExtension] = useState<string>('')
+  const [loading, setLoading] = useState<boolean>(false)
 
-  const image1 = "https://images.ecycle.com.br/wp-content/uploads/2021/05/20195924/o-que-e-paisagem.jpg.webp"
-  const image2 = "https://cursinhoparamedicina.com.br/wp-content/uploads/2022/10/Paisagem-1.jpg"
+  async function searchImage(){
+    setLoading(true)
+    const result = await useService.buscar(query, extension)
+    setImages(result)
+    setLoading(false)
+    
+  }
 
-  const [codigoImagem, setCodigoImagem] = useState<number>(1)
-  const [urlImage, setUrlImage] = useState<string>(image1)
+  function renderImageCard(image: Image){
+    return (
+      <ImageCard 
+      nome={image.name} src={image.url} 
+      tamanho={image.size} dataUpload={image.uploadDate} 
+      extension={image.extension}
+      key={image.url}/>
+    ) 
+  }
 
-  function mudarImager(){
-    if(codigoImagem == 1){
-      setCodigoImagem(2)
-      setUrlImage(image2)
-    } else {
-      setCodigoImagem(1)
-      setUrlImage(image1)
-    }
+  function renderImageCards(){
+    return images.map(renderImageCard)
   }
 
   return (
-    <Template>
-      <button onClick={mudarImager} >Clique para mudar</button>
+    <Template loading={loading}>
+        <section className="flex flex-col items-center justify-center my-5">
+          <div className="flex space-x-4">
+            <input type="text"
+            onChange={(e) => setQuery(e.target.value)}
+            className="border px-5 py-2 rounded-lg text-gray-900 "/>
+            <select onChange={(e) => setExtension(e.target.value)} className="border px-4 py-2 rounded-lg text-gray-900 ">
+              <option value="">All formats</option>
+              <option value="PNG">PNG</option>
+              <option value="JPEG">JPEG</option>
+              <option value="GIF">GIF</option>
+            </select>
+            <button onClick={searchImage} className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-300">Search</button>
+            <button className="bg-yellow-500 text-white px-4 py-2 rounded-lg hover:bg-yellow-300">Add new</button>
+          </div>
+        </section>
       <section className="grid grid-cols-3 gap-8">
-        <ImageCard dataUpload="18/06/2024" src={urlImage} nome="Paisagem" tamanho="10mb" />
-        <ImageCard dataUpload="18/06/2024" src={urlImage} nome="Paisagem" tamanho="10mb" />
-        <ImageCard dataUpload="18/06/2024" src={urlImage} nome="Paisagem" tamanho="10mb" />
-        <ImageCard dataUpload="18/06/2024" src={urlImage} nome="Paisagem" tamanho="10mb" />
-        <ImageCard dataUpload="18/06/2024" src={urlImage} nome="Paisagem" tamanho="10mb" />
+          {
+            renderImageCards()
+          }
       </section>
     </Template>
   )
